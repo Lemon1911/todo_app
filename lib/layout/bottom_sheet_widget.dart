eb6e46762a787e5da1b809ae13286d1ecfe8f520
Lemon1911 <m7moud.laymouna.ml@gmail.com>
2023-11-25 15:31:18 +0200

    Bottom sheet update

--- lib/layout/bottom_sheet_widget.dart
+++ lib/layout/bottom_sheet_widget.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/moduls/widgets/custom_text_form_filed.dart';
 
 class BottomSheetWidget extends StatelessWidget {
   BottomSheetWidget({super.key});
 
   TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  var formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -11,57 +14,86 @@ class BottomSheetWidget extends StatelessWidget {
 
     return Padding(
       padding: const EdgeInsets.all(20.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text(
-            "Add New Task",
-            textAlign: TextAlign.center,
-            style: theme.textTheme.titleLarge!.copyWith(
-              color: Colors.black,
-            ),
-          ),
-          TextFormField(
-            controller: titleController,
-            onChanged: (String value) {},
-            decoration: InputDecoration(
-              hintText: "Enter your task title",
-            ),
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              hintText: "Enter your task description",
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                "Add New Task",
-                style: theme.textTheme.titleLarge!.copyWith(
-                  color: Colors.black,
-                ),
-              ),
-              Text(
-                "5 oct 2023",
-                textAlign: TextAlign.center,
-                style: theme.textTheme.bodyMedium!.copyWith(
-                  color: theme.primaryColor,
-                ),
-              ),
-            ],
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text(
-              "Add Task",
+      child: Form(
+        child: Column(
+          key: formKey,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              "Add New Task",
+              textAlign: TextAlign.center,
               style: theme.textTheme.titleLarge!.copyWith(
-                color: Colors.white,
+                color: Colors.black,
               ),
             ),
-          ),
-        ],
+            CustomTextFormFiled(
+              title: "Enter your task title",
+              controller: titleController,
+              validator: (value) {
+                if (value == null || value.trim().isEmpty) {
+                  return "you must provide task";
+                } else if (value.length < 10) {
+                  return "you must provide task";
+                } else {
+                  return null;
+                }
+              },
+            ),
+            CustomTextFormFiled(
+                title: "Enter your task description",
+                controller: descriptionController),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  "Add New Task",
+                  style: theme.textTheme.titleLarge!.copyWith(
+                    color: Colors.black,
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    showCalendar(context);
+                  },
+                  child: Text(
+                    "5 oct 2023",
+                    textAlign: TextAlign.center,
+                    style: theme.textTheme.bodyMedium!.copyWith(
+                      color: theme.primaryColor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (formKey.currentState!.validate()) {
+                  print(titleController.text);
+                }
+              },
+              child: Text(
+                "Add Task",
+                style: theme.textTheme.titleLarge!.copyWith(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void showCalendar(context) {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime.now().add(
+        const Duration(
+          days: 365,
+        ),
       ),
     );
   }
